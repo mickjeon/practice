72aedc011713d76bd6e21a9a363c6cdae1830f36
Mick <mickjeon@gmail.com>
2022-05-01 20:53:39 -0700

    updated

--- PracticeTester.java
+++ PracticeTester.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class PracticeTester {
     @Test
     public void testMultiply() {
-        assertEquals(0, Practice.multiply(2, 5));
+        assertEquals(10, Practice.multiply(2, 5));
     }
 }
